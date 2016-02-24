c8e63ad6f08e8decea73258a2b1ec852140edc7e
HugoSilveiro <hugofilipe95@hotmail.com>
2016-02-24 14:43:11 +0000

    LightSpawner Script

---
+++ TP1 IIA/Assets/Scripts/LightSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int maxNumberLights = 10;
+		int xRandom, zRandom;
+		for(int i = 0; i < maxNumberLights; i++){
+				xRandom = Random.Range(-15,15);
+				zRandom = Random.Range(-15,15);
+				GameObject newLight = Instantiate(Resources.Load("Point light"),new Vector3(xRandom,1,zRandom), Quaternion.identity) as GameObject;
+				newLight.transform.parent = this.gameObject.transform;
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
