4efcc2765ba5f67143e485a7c4761439e06518cc
j-godinho <joaogodinho_4@hotmail.com>
2016-02-28 14:31:32 +0000

    The beggining of xuning the car 3a

---
+++ TP1 IIA/Assets/Scripts/CarBehaviour3a.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour3a : CarBehaviour {
+
+	public ObjectDetector leftOD;
+	public ObjectDetector rightOD;
+	private Rigidbody rb;
+
+
+	void Update()
+	{
+		//Read sensor values
+
+		float lightLeftSensor = LeftLD.getLinearOutput ();
+		float lightRightSensor = RightLD.getLinearOutput ();
+
+		//Calculate target motor values
+		m_LeftWheelSpeed = lightLeftSensor * MaxSpeed;
+		m_RightWheelSpeed = lightRightSensor * MaxSpeed;
+
+		leftOD.angle = 20;
+		rightOD.angle = 20;
+
+		//Calculate target motor values
+
+		bool leftSensor = leftOD.getOutput();
+		bool rightSensor = rightOD.getOutput();
+
+		if (leftSensor && !rightSensor) {
+			//go right
+			m_LeftWheelSpeed = m_LeftWheelSpeed + 5;
+			m_RightWheelSpeed = m_RightWheelSpeed - 5;
+		} else if (rightSensor && !leftSensor) {
+			//go left
+			m_RightWheelSpeed = m_RightWheelSpeed + 5;
+			m_LeftWheelSpeed = m_LeftWheelSpeed - 5;
+
+		} 
+		else {
+			m_LeftWheelSpeed = m_LeftWheelSpeed + 5;
+			m_RightWheelSpeed = m_RightWheelSpeed +5;
+		}
+
+	}
+
+}
