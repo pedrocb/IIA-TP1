0687659c8e1cc2f4770dbce1e21d037459cbeffc
j-godinho <joaogodinho_4@hotmail.com>
2016-02-28 22:49:24 +0000

    inversamente proporcional

--- TP1 IIA/Assets/Scripts/CarBehaviour3b.cs
+++ TP1 IIA/Assets/Scripts/CarBehaviour3b.cs
@@ -20,8 +20,8 @@ public class CarBehaviour3b : CarBehaviour {
 	//A velocidade das rodas é inversamente proporcional ao output dos sensores
 	//Sensor da direita influencia roda da esquerda e vice-versa
 	Debug.Log(leftSensor);
-	m_LeftWheelSpeed = (leftSensor) * MaxSpeed;
-	m_RightWheelSpeed = (rightSensor) * MaxSpeed;
+	m_LeftWheelSpeed = (1/leftSensor) * MaxSpeed;
+	m_RightWheelSpeed = (1/rightSensor) * MaxSpeed;
 
 
 	//Calcular os outputs dos sensores
