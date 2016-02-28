b7cf11688bee7e44566c4d5c068c880470738c74
HugoSilveiro <hugofilipe95@hotmail.com>
2016-02-28 19:36:31 +0000

    Prefabs ok

--- TP1 IIA/Assets/Scripts/CarBehaviour3a.cs
+++ TP1 IIA/Assets/Scripts/CarBehaviour3a.cs
@@ -1,47 +1,132 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CarBehaviour3a : CarBehaviour {
 
-	public ObjectDetector leftOD;
-	public ObjectDetector rightOD;
-	private Rigidbody rb;
+    public ObjectDetector northOD;
+    public ObjectDetector southOD;
+    public ObjectDetector eastOD;
+    public ObjectDetector westOD;
+    private Rigidbody rb;
 
 
-	void Update()
-	{
-		//Read sensor values
+    void Update()
+    {
+	//Read sensor values
+	float leftSensor = LeftLD.getLinearOutput ();
+	float rightSensor = RightLD.getLinearOutput ();
 
-		float lightLeftSensor = LeftLD.getLinearOutput ();
-		float lightRightSensor = RightLD.getLinearOutput ();
+	//Calculate target motor values
+	m_LeftWheelSpeed = leftSensor * MaxSpeed;
+	m_RightWheelSpeed = rightSensor * MaxSpeed;
 
-		//Calculate target motor values
-		m_LeftWheelSpeed = lightLeftSensor * MaxSpeed;
-		m_RightWheelSpeed = lightRightSensor * MaxSpeed;
 
-		leftOD.angle = 20;
-		rightOD.angle = 20;
+	//Calculate target motor values
 
-		//Calculate target motor values
+        bool northSensor = northOD.getOutput();
+        bool southSensor = southOD.getOutput();
+        bool eastSensor = eastOD.getOutput();
+        bool westSensor = westOD.getOutput();
 
-		bool leftSensor = leftOD.getOutput();
-		bool rightSensor = rightOD.getOutput();
+        if(northSensor){
+            if(!eastSensor){
+                //m_RightWheelSpeed = m_RightWheelSpeed +20;
+                transform.root.transform.Rotate(0,10,0);
+            }
+            else if(!westSensor){
+                //m_LeftWheelSpeed = m_LeftWheelSpeed + 20;
+                transform.root.transform.Rotate(0,-10,0);
+            }
+            else if(!southSensor){
+                //m_RightWheelSpeed = m_RightWheelSpeed - 20;
+                //m_LeftWheelSpeed = m_LeftWheelSpeed - 20;
+                transform.root.transform.Rotate(0,180,0);
+            }
+        }
 
-		if (leftSensor && !rightSensor) {
-			//go right
-			m_LeftWheelSpeed = m_LeftWheelSpeed + 5;
-			m_RightWheelSpeed = m_RightWheelSpeed - 5;
-		} else if (rightSensor && !leftSensor) {
-			//go left
-			m_RightWheelSpeed = m_RightWheelSpeed + 5;
-			m_LeftWheelSpeed = m_LeftWheelSpeed - 5;
+        if(westSensor){
+            if(!northSensor){
+                transform.root.transform.Rotate(0,0,0);
+                //m_LeftWheelSpeed = m_LeftWheelSpeed;
+                //m_RightWheelSpeed = m_RightWheelSpeed;
 
-		}
-		else {
-			m_LeftWheelSpeed = m_LeftWheelSpeed + 5;
-			m_RightWheelSpeed = m_RightWheelSpeed +5;
-		}
+            }
+            else if(!eastSensor){
+                //m_RightWheelSpeed = m_RightWheelSpeed + 20;
+                transform.root.transform.Rotate(0,10,0);
+            }
+            else if(!southSensor){
+                //m_RightWheelSpeed = m_RightWheelSpeed - 20;
+                //m_LeftWheelSpeed = m_LeftWheelSpeed - 20;
+                transform.root.transform.Rotate(0,180,0);
+            }
+        }
 
-	}
+
+        if(eastSensor){
+            if(!northSensor){
+                //m_LeftWheelSpeed = m_LeftWheelSpeed + 20;
+                //m_RightWheelSpeed = m_RightWheelSpeed;
+                transform.root.transform.Rotate(0,0,0);
+            }
+            else if(!westSensor){
+                //m_LeftWheelSpeed = m_LeftWheelSpeed + 20;
+                transform.root.transform.Rotate(0,-10,0);
+            }
+            else if(!southSensor){
+                //m_RightWheelSpeed = m_RightWheelSpeed - 20;
+                //m_LeftWheelSpeed = m_LeftWheelSpeed - 20;
+                transform.root.transform.Rotate(0,180,0);
+            }
+
+        }
+        if(southSensor){
+            if(!eastSensor){
+                m_LeftWheelSpeed = m_LeftWheelSpeed + 20;
+            }
+            else if(!eastSensor){
+                m_RightWheelSpeed = m_RightWheelSpeed - 20;
+            }
+            else if(!northSensor){
+                m_LeftWheelSpeed = m_LeftWheelSpeed;
+                m_RightWheelSpeed = m_RightWheelSpeed;
+
+            }
+
+
+        }
+
+        /*
+        if(transform.position.x == -22 && transform.position.z< 22 && transform.position.z>-22)
+        {
+            Debug.Log("PAREDE 1");
+            transform.root.transform.Rotate(0, 180, 0);
+        }
+        else if (transform.position.x == 22 && transform.position.z < 22 && transform.position.z > -22)
+        {
+            Debug.Log("PAREDE 2");
+            transform.root.transform.Rotate(0, 180, 0);
+        }
+        else if (transform.position.z == -22 && transform.position.x < 22 && transform.position.x > -22)
+        {
+            Debug.Log("PAREDE 3");
+            transform.root.transform.Rotate(0, 180, 0);
+        }
+        else if (transform.position.z == -22 && transform.position.x < 22 && transform.position.x > -22)
+        {
+            Debug.Log("PAREDE 4");
+            transform.root.transform.Rotate(0, 180, 0);
+        }
+        */
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cube"))
+        {
+
+        }
+    }
 
 }
