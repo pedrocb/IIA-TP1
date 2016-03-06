e2e371151b6c9185eec60c5ffda2bbec24098461
pedrocb <pedrocb_c-s@hotmail.com>
2016-03-06 17:35:41 +0000

    Fixed object detector

--- TP1 IIA/Assets/Scripts/CarBehaviour2a.cs
+++ TP1 IIA/Assets/Scripts/CarBehaviour2a.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class CarBehaviour2a : CarBehaviour {
 
-    public ObjectDetector northOD;
-    public ObjectDetector southOD;
     public ObjectDetector eastOD;
     public ObjectDetector westOD;
     private Rigidbody rb;
@@ -15,67 +13,20 @@ public class CarBehaviour2a : CarBehaviour {
 	//Read sensor values
 	float leftSensor = LeftLD.getLinearOutput ();
 	float rightSensor = RightLD.getLinearOutput ();
-	Debug.Log(leftSensor + " " + rightSensor);
 	//Calculate target motor values
 	m_LeftWheelSpeed = leftSensor * MaxSpeed;
 	m_RightWheelSpeed = rightSensor * MaxSpeed;
-
-
+	
+	float westObjectSensor = westOD.getOutput();
+	float eastObjectSensor = eastOD.getOutput();
+	Debug.Log(westObjectSensor + " " +eastObjectSensor);
+	
+	m_LeftWheelSpeed+= (1/westObjectSensor) * MaxSpeed;
+	m_RightWheelSpeed+= (1/eastObjectSensor) * MaxSpeed;
+	
 	//Calcular os outputs dos sensores
-        bool northSensor = northOD.getOutput();
-        bool southSensor = southOD.getOutput();
-        bool eastSensor = eastOD.getOutput();
-        bool westSensor = westOD.getOutput();
-
+	
 	//Conforme os outputs rodar o carro
-        if(northSensor){
-            if(!eastSensor){
-                transform.root.transform.Rotate(0,10,0);
-            }
-            else if(!westSensor){
-                transform.root.transform.Rotate(0,-10,0);
-            }
-            else if(!southSensor){
-                transform.root.transform.Rotate(0,180,0);
-            }
-        }
-
-        if(westSensor){
-            if(!northSensor){
-                transform.root.transform.Rotate(0,0,0);
-
-            }
-            else if(!eastSensor){
-                transform.root.transform.Rotate(0,10,0);
-            }
-            else if(!southSensor){
-                transform.root.transform.Rotate(0,180,0);
-            }
-        }
-
-
-        if(eastSensor){
-            if(!northSensor){
-                transform.root.transform.Rotate(0,0,0);
-            }
-            else if(!westSensor){
-                transform.root.transform.Rotate(0,-10,0);
-            }
-            else if(!southSensor){
-                transform.root.transform.Rotate(0,180,0);
-            }
-
-        }
-
-        if(southSensor){
-            if(!eastSensor){
-                transform.root.transform.Rotate(0,10,0);
-	    }
-            else if(!westSensor){
-                transform.root.transform.Rotate(0,-10,0);
-            }
-
-        }
     }
 
     void OnTriggerEnter(Collider other)
